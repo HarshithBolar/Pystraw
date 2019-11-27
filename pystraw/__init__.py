bdb12363924250e0fcb90594cb166b14b77196ea
Harshith Bolar <harshith@localhost.localdomain>
2019-11-27 22:35:32 +0530

    Initial commit

---
+++ pystraw/__init__.py
@@ -0,0 +1,7 @@
+"""
+Python Strimzi-Bridge API Wrapper.
+PYSTRAW, an acronym for "Python Strimzi-Bridge API Wrapper", is a python package that
+allows for simple access to Kafka Strimzi-Bridge's API.
+
+More information about Kafka Strimzi-Bidge can be found at https://github.com/strimzi/strimzi-kafka-bridge
+"""
