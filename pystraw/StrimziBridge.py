a7ccceb41a5d302f7d7b059c0de201a319572a70
HarshithBolar <harshith.bolar@gmail.com>
2019-11-28 01:04:55 +0530

    Added subscribe API

--- pystraw/StrimziBridge.py
+++ pystraw/StrimziBridge.py
@@ -1,19 +1,19 @@
 import requests
 
+
 class StrimziBridge:
     def __init__(self, domain_name, port):
         self._base_url = f"http://{domain_name}:{port}"
-        self.domain_name = domain_name
-        self.port = port
 
-    def _make_api_call(self, http_method, url, headers, kwargs):
+    @staticmethod
+    def _make_api_call(http_method, url, headers, kwargs):
         response = None
         if http_method == "POST":
             response = requests.post(url, json=kwargs, headers=headers)
 
         if response is not None:
             print(response.status_code)
-            print(response.json())
+            print(response.content)
 
     def create_consumer(self, group_name, **kwargs):
         url = f"{self._base_url}/consumers/{group_name}"
@@ -23,5 +23,11 @@ class StrimziBridge:
         for key, value in kwargs.items():
             if "_" in key:
                 kwargs[key.replace("_", ".")] = kwargs.pop(key)
+
+        self._make_api_call("POST", url, header, kwargs)
+
+    def subscribe(self, group_name, consumer_name, **kwargs):
+        url = f"{self._base_url}/consumers/{group_name}/instances/{consumer_name}/subscription"
+        header = {"content-type": "application/vnd.kafka.v2+json"}
 
         self._make_api_call("POST", url, header, kwargs)
