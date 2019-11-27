51300684e180d04cadc56e7f864aed7f643e1b32
HarshithBolar <harshith.bolar@gmail.com>
2019-11-28 02:38:59 +0530

    refactoring

--- pystraw/StrimziBridge.py
+++ pystraw/StrimziBridge.py
@@ -15,19 +15,24 @@ class StrimziBridge:
             print(response.status_code)
             print(response.content)
 
-    def create_consumer(self, group_name, **kwargs):
+    def create_consumer(self, group_name, consumer_name=None, **kwargs):
         url = f"{self._base_url}/consumers/{group_name}"
         header = {"content-type": "application/vnd.kafka.v2+json"}
+        body = {}
+
+        if consumer_name is not None:
+            body['name'] = consumer_name
 
         # Change underscores to Kafka property names with periods
         for key, value in kwargs.items():
             if "_" in key:
-                kwargs[key.replace("_", ".")] = kwargs.pop(key)
+                body[key.replace("_", ".")] = kwargs[key]
 
-        self._make_api_call("POST", url, header, kwargs)
+        self._make_api_call("POST", url, header, body)
 
-    def subscribe(self, group_name, consumer_name, **kwargs):
+    def subscribe(self, group_name, consumer_name, topics):
         url = f"{self._base_url}/consumers/{group_name}/instances/{consumer_name}/subscription"
         header = {"content-type": "application/vnd.kafka.v2+json"}
+        body = {"topics": topics}
 
-        self._make_api_call("POST", url, header, kwargs)
+        self._make_api_call("POST", url, header, body)
