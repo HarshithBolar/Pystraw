0ab12fcdd8be23984c094db71adde38697325663
HarshithBolar <harshith.bolar@gmail.com>
2019-11-28 14:28:33 +0530

    Added /records API

--- pystraw/StrimziBridge.py
+++ pystraw/StrimziBridge.py
@@ -4,20 +4,24 @@ import requests
 class StrimziBridge:
     def __init__(self, domain_name, port):
         self._base_url = f"http://{domain_name}:{port}"
+        self._json_post_header = {"content-type": "application/vnd.kafka.v2+json"}
+        self._json_accept_header = {"accept": "application/vnd.kafka.json.v2+json"}
+        self._binary_accept_header = {"accept": "application/vnd.kafka.binary.v2+json"}
 
     @staticmethod
-    def _make_api_call(http_method, url, headers, kwargs):
+    def _make_api_call(http_method, url, headers, body=None):
         response = None
-        if http_method == "POST":
-            response = requests.post(url, json=kwargs, headers=headers)
 
-        if response is not None:
-            print(response.status_code)
-            print(response.content)
+        if http_method == "GET":
+            response = requests.get(url, headers=headers)
+        elif http_method == "POST":
+            response = requests.post(url, json=body, headers=headers)
+
+        return response
 
     def create_consumer(self, group_name, consumer_name=None, **kwargs):
         url = f"{self._base_url}/consumers/{group_name}"
-        header = {"content-type": "application/vnd.kafka.v2+json"}
+        header = self._json_post_header
         body = {}
 
         if consumer_name is not None:
@@ -27,12 +31,26 @@ class StrimziBridge:
         for key, value in kwargs.items():
             if "_" in key:
                 body[key.replace("_", ".")] = kwargs[key]
+            else:
+                body[key] = kwargs[key]
 
-        self._make_api_call("POST", url, header, body)
+        return self._make_api_call("POST", url, header, body)
 
     def subscribe(self, group_name, consumer_name, topics):
         url = f"{self._base_url}/consumers/{group_name}/instances/{consumer_name}/subscription"
-        header = {"content-type": "application/vnd.kafka.v2+json"}
+        header = self._json_post_header
         body = {"topics": topics}
 
-        self._make_api_call("POST", url, header, body)
+        return self._make_api_call("POST", url, header, body)
+
+    def read_records(self, group_name, consumer_name, msg_format):
+        url = f"{self._base_url}/consumers/{group_name}/instances/{consumer_name}/records"
+
+        if msg_format == "json":
+            header = self._json_accept_header
+        elif msg_format == "binary":
+            header = self._binary_accept_header
+        else:
+            raise ValueError("Invalid 'msg_version'. Valid are 'json' or 'binary'.")
+
+        return self._make_api_call("GET", url, header)
